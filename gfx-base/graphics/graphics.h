f3ae0da4b88cc4f3b6d043c9b3d1c574700e3689
Aleksander <servaL24@i.ua>
2026-05-30 22:48:50 +0300

    2026-05-30

--- gfx-base/graphics/graphics.h
+++ gfx-base/graphics/graphics.h
@@ -1,5 +1,14 @@
 // graphics.h — тепер просто "агрегатор"
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
+
 #include "gfx.h"  // А gfx.h вже включає lines.h + rects.h
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Vector2 {
+    float x;
+    float y;
+} Vector2;
+
 #endif
